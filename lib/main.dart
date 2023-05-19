009db2e9a205c19fccfd60b45479c6068a8e7576
Sanjar17 <sanjarsaidov173@gmail.com>
2023-05-19 23:22:09 +0500

    file picker added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,163 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
+import 'package:direct_sms/direct_sms.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var directSms = DirectSms();
+  List numbers = [];
+  String label = "No file selected";
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  _sendSms({required String number, required String message}) async {
+    final permission = Permission.sms.request();
+    if (await permission.isGranted) {
+      directSms.sendSms(message: message, phone: number);
+      print('Message sent');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    // var phoneController = TextEditingController();
+    var messageController = TextEditingController();
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Message sender app'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  // a button to pick a file
+                  Expanded(
+                    flex: 3,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        // backgroundColor: Colors.white,
+                        // foregroundColor: const Color.fromARGB(255, 146, 146, 146),
+                        minimumSize: Size(40, 60),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Color.fromARGB(255, 150, 149, 149), width: 1),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: () async {
+                        // pick a file
+                        FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+                        if (result != null) {
+                          File file = File(result.files.single.path!);
+                          var map = jsonDecode(file.readAsStringSync());
+
+                          print(map);
+                          print(map.runtimeType);
+                          // change the label
+                          setState(() {
+                            numbers = map;
+                            label = result.files.single.name;
+                          });
+                        } else {
+                          // User canceled the picker
+                        }
+                      },
+                      child: const Text("Pick a file"),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 7,
+                    child: TextField(
+                      enabled: false,
+                      // controller: phoneController,
+                      // keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        label: Text(label),
+                        border: const OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: messageController,
+                // make input decoration bigger
+                maxLines: 5,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Message",
+                ),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(40, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () async {
+                    if (numbers.isNotEmpty) {
+                      for (var number in numbers) {
+                        try {
+                          _sendSms(message: messageController.text, number: number["phone"].trim());
+                          // wait for 1 second
+                          await Future.delayed(const Duration(seconds: 1));
+                          // setState(() {
+                          //   counter++;
+                          // });
+                        } catch (e) {
+                          print(e);
+                        }
+                      }
+                      // setState(() {
+                      //   counter = 0;
+                      // });
+                    } else {
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text("Error"),
+                              content: const Text("Please select a file"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("OK"))
+                              ],
+                            );
+                          });
+                    }
+                  },
+                  child: Text(counter == 0 ? "Send" : "Send $counter"))
+            ],
+          ),
         ),
       ),
     );
